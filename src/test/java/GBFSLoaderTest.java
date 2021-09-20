ce80c6989612e580a9de6ad309ceecfc753d54f8
Tom Erik Støwer <testower@gmail.com>
2021-09-20 15:52:02 +0200

    Fix compiler issues

--- src/test/java/GBFSLoaderTest.java
+++ src/test/java/GBFSLoaderTest.java
@@ -9,11 +9,11 @@ public class GBFSLoaderTest {
 
     @Test
     void testLoader() throws URISyntaxException, InterruptedException {
-        var loader = new GbfsSubscriptionManager();
-        var options = new GbfsSubscriptionOptions();
+        GbfsSubscriptionManager loader = new GbfsSubscriptionManager();
+        GbfsSubscriptionOptions options = new GbfsSubscriptionOptions();
         options.setDiscoveryURI(new URI("https://api.dev.entur.io/mobility/v2/gbfs/limeoslo/gbfs.json"));
         options.setLanguageCode("nb");
-        var subscriber = loader.subscribe(options, delivery -> {
+        String subscriber = loader.subscribe(options, delivery -> {
            System.out.println(System.currentTimeMillis() + " " + delivery.getFreeBikeStatus().getData().getBikes().size());
         });
 
