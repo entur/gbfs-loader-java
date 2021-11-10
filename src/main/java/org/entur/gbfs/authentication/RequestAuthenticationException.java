0a896e0db79de0f03ccb2b4584d8bb076a2b18e4
Tom Erik Støwer <testower@gmail.com>
2021-11-10 14:30:07 +0100

    Add missing file

---
+++ src/main/java/org/entur/gbfs/authentication/RequestAuthenticationException.java
@@ -0,0 +1,7 @@
+package org.entur.gbfs.authentication;
+
+public class RequestAuthenticationException extends RuntimeException {
+    public RequestAuthenticationException(Throwable e) {
+        super(e);
+    }
+}
