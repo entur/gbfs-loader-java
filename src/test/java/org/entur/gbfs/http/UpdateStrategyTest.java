b00759c5392422980302214af2d38dd88a89f739
Tom Erik Støwer <testower@gmail.com>
2024-03-21 15:18:59 +0100

    Add unit tests for UpdateStrategy

---
+++ src/test/java/org/entur/gbfs/http/UpdateStrategyTest.java
@@ -0,0 +1,37 @@
+package org.entur.gbfs.http;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.time.Instant;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+public class UpdateStrategyTest {
+
+  @Test
+  void testUpdateImmediately() {
+    UpdateStrategy subject = new UpdateStrategy();
+    subject.scheduleNextUpdate((int) Instant.now().getEpochSecond(), 0);
+    assertTrue(subject.shouldUpdate());
+  }
+
+  @Test
+  void testUpdateLater() {
+    UpdateStrategy subject = new UpdateStrategy();
+    subject.scheduleNextUpdate((int) Instant.now().getEpochSecond(), 10);
+    assertFalse(subject.shouldUpdate());
+  }
+
+  @Test
+  @Disabled(
+    "This test is flaky due to rescheduleAfterFailure does not always leave enough time for the next assertion"
+  )
+  void testUpdateFailed() {
+    UpdateStrategy subject = new UpdateStrategy();
+    subject.scheduleNextUpdate((int) Instant.now().getEpochSecond(), 0);
+    assertTrue(subject.shouldUpdate());
+    subject.rescheduleAfterFailure();
+    assertFalse(subject.shouldUpdate());
+  }
+}
