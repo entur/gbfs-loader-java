bb3e074995181091d2725074563abea5d4145acd
Tom Erik Støwer <testower@gmail.com>
2024-03-22 17:21:49 +0100

    Add v3 subscription test

--- src/test/java/org/entur/gbfs/GBFSSubscriptionTest.java
+++ src/test/java/org/entur/gbfs/GBFSSubscriptionTest.java
@@ -6,6 +6,7 @@ import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ForkJoinPool;
 import java.util.function.Consumer;
 import org.entur.gbfs.loader.v2.GbfsV2Delivery;
+import org.entur.gbfs.loader.v3.GbfsV3Delivery;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -64,6 +65,37 @@ class GBFSSubscriptionTest {
     GbfsSubscriptionOptions options = new GbfsSubscriptionOptions();
     options.setDiscoveryURI(new URI(url));
     options.setLanguageCode(languageCode);
+    options.setEnableValidation(true);
+    return options;
+  }
+
+  @Test
+  void testV3Subscription() throws URISyntaxException, InterruptedException {
+    waiter = new CountDownLatch(1);
+    GbfsSubscriptionManager loader = new GbfsSubscriptionManager();
+    String subscriber = loader.subscribeV3(
+      getV3TestOptions("file:src/test/resources/gbfs/v3/getaroundstavanger/gbfs.json"),
+      getV3TestConsumer()
+    );
+    loader.update();
+    waiter.await();
+    loader.unsubscribe(subscriber);
+  }
+
+  Consumer<GbfsV3Delivery> getV3TestConsumer() {
+    return delivery -> {
+      Assertions.assertNotNull(delivery);
+      Assertions.assertEquals(
+        0,
+        delivery.validationResult().getSummary().getErrorsCount()
+      );
+      waiter.countDown();
+    };
+  }
+
+  GbfsSubscriptionOptions getV3TestOptions(String url) throws URISyntaxException {
+    GbfsSubscriptionOptions options = new GbfsSubscriptionOptions();
+    options.setDiscoveryURI(new URI(url));
     options.setEnableValidation(true);
     return options;
   }
