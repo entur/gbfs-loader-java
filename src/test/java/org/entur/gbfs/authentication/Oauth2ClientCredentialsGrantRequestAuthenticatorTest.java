fc744d2b4cb02f33c4c11319b776970edb398087
Tom Erik Støwer <tom.erik.stower@entur.org>
2024-04-09 21:21:45 +0200

    Add failing test that proves non thread safety, and incorrect implementation
    We checked expiration was after current time instead of before...

--- src/test/java/org/entur/gbfs/authentication/Oauth2ClientCredentialsGrantRequestAuthenticatorTest.java
+++ src/test/java/org/entur/gbfs/authentication/Oauth2ClientCredentialsGrantRequestAuthenticatorTest.java
@@ -7,6 +7,7 @@ import com.github.tomakehurst.wiremock.junit5.WireMockTest;
 import java.net.URI;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.CountDownLatch;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -16,7 +17,7 @@ class Oauth2ClientCredentialsGrantRequestAuthenticatorTest {
   @Test
   void testOauth2ClientCredentialsGrantRequestAuthenticator(
     WireMockRuntimeInfo runtimeInfo
-  ) {
+  ) throws InterruptedException {
     String TEST_TOKEN_URL = "http://localhost:" + runtimeInfo.getHttpPort() + "/token";
     String TEST_USER = "foo";
     String TEST_PASSWORD = "bar";
@@ -25,8 +26,9 @@ class Oauth2ClientCredentialsGrantRequestAuthenticatorTest {
       post("/token")
         .withRequestBody(equalTo("grant_type=client_credentials&scope=test-scope"))
         .withBasicAuth(TEST_USER, TEST_PASSWORD)
-        .willReturn(okJson("{\"access_token\":\"fake_token\"}"))
+        .willReturn(okJson("{\"access_token\":\"fake_token\", \"expires_in\":3600}"))
     );
+
     Oauth2ClientCredentialsGrantRequestAuthenticator authenticator =
       new Oauth2ClientCredentialsGrantRequestAuthenticator(
         URI.create(TEST_TOKEN_URL),
@@ -34,8 +36,31 @@ class Oauth2ClientCredentialsGrantRequestAuthenticatorTest {
         TEST_PASSWORD,
         "test-scope"
       );
-    Map<String, String> headers = new HashMap<>();
-    authenticator.authenticateRequest(headers);
-    Assertions.assertEquals("Bearer fake_token", headers.get("Authorization"));
+
+    CountDownLatch latch = new CountDownLatch(2);
+
+    Thread thread1 = new Thread(() -> assertAuthenticateRequest(authenticator, latch));
+    Thread thread2 = new Thread(() -> assertAuthenticateRequest(authenticator, latch));
+    thread1.start();
+    thread2.start();
+
+    latch.await();
+
+    verify(exactly(1), postRequestedFor(urlEqualTo("/token")));
+  }
+
+  private void assertAuthenticateRequest(
+    RequestAuthenticator authenticator,
+    CountDownLatch latch
+  ) {
+    try {
+      Map<String, String> headers = new HashMap<>();
+      authenticator.authenticateRequest(headers);
+      Assertions.assertEquals("Bearer fake_token", headers.get("Authorization"));
+    } catch (RequestAuthenticationException e) {
+      Assertions.fail(e.getMessage());
+    } finally {
+      latch.countDown();
+    }
   }
 }
