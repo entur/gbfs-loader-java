505104a47aa8cf48465b5c889a38f8ab24bd2daf
Holger Bruch <hb@mfdz.de>
2024-04-20 15:40:14 +0200

    fix: return raw discovery feed

--- src/main/java/org/entur/gbfs/loader/v2/GbfsV2Loader.java
+++ src/main/java/org/entur/gbfs/loader/v2/GbfsV2Loader.java
@@ -125,4 +125,9 @@ public class GbfsV2Loader extends BaseGbfsLoader<GBFSFeedName, GBFS> {
       )
       .collect(Collectors.toList());
   }
+
+  @Override
+  protected GBFSFeedName getDiscoveryFeedName() {
+    return GBFSFeedName.GBFS;
+  }
 }
